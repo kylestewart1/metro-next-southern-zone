ac591e679104fdc36d07934da1860ec072af184c
kylestewart1 <98936057+kylestewart1@users.noreply.github.com>
2024-11-22 14:26:24 -0500

    added routes and stops reachable through METRO NEXT, with info popups when clicked

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import "./styles.css";
 import "leaflet/dist/leaflet.css";
-import L, { marker } from "leaflet";
+import L, { latLng, marker } from "leaflet";
 import { GeoSearchControl, OpenStreetMapProvider } from "leaflet-geosearch";
 import zoneData from "../assets/Southern_Zone_WGS_1984.geojson";
+import stops from "../assets/southern_zone_stops_WGS_1984.geojson";
+import routes from "../assets/southern_zone_nearby_routes_WGS_1984.geojson";
 import markerIcon from "../assets/marker-icon.png";
 
 const icon = new L.icon({
@@ -43,3 +45,51 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Add zone polygon layer
 L.geoJSON(zoneData).addTo(map);
+
+
+// Add routes you'll be able to reach directly from METRO NEXT
+
+function routeInfo(feature, layer) {
+    if (feature.properties) {
+        const info = `Route ${feature.properties.VAR_ROUTE} ` + 
+                     (feature.properties.VAR_DIREC === 4 ? "Inbound" : "Outbound");
+        layer.bindPopup(info);
+    }
+}
+L.geoJSON(routes, {
+    color: "red",
+    onEachFeature: routeInfo
+}).addTo(map);
+
+
+
+
+// Add bus stops within 3/4 mile of zone
+
+// marker settings for bus stops
+const geojsonMarkerOptions = {
+    radius: 4,
+    fillcolor: "#fff",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+}
+
+// Pop up with stop info when clicked
+function stopInfo(feature, layer) {
+    if (feature.properties) {
+        const info = `Route ${feature.properties.Route}, ` +
+                     `${feature.properties.Direction}, ` +
+                     `Stop #${feature.properties.Stop} - ${feature.properties.Description}`;
+        layer.bindPopup(info);
+    }
+}
+
+L.geoJSON(stops, {
+    pointToLayer: (feature, latlng) => {
+        return L.circleMarker(latlng, geojsonMarkerOptions)
+    },
+    onEachFeature: stopInfo
+}).addTo(map);
+
